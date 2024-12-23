65162e31150cc27ac03c67eb7dec087c4edef5b0
91902 <91902@SONU>
2024-12-23 21:52:03 +0530

    Add project files.

---
+++ CrudWebApi/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudWebApi.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        [Required] 
+        public string studentName { get; set; }
+        [Required] 
+        public string studentGender { get; set; }
+        [Required] 
+        public int age { get; set; }
+        [Required] 
+        public int standard { get; set; }
+    }
+
+
+}
